36617778a84f12d0a438bb94ad3a37ed433c1137
Thanh Tuyen <dttuyen260701@gmail.com>
2023-04-23 22:10:52 +0700

    add MoreData

--- CrawlData/TestLocation.py
+++ CrawlData/TestLocation.py
@@ -1,6 +1,6 @@
 from geopy.geocoders import Nominatim
 location = Nominatim(user_agent="GetLoc", timeout = 10000)
-getLocation = location.geocode("15 Lo Giang 19, Hoa xuan, Cam le")
+getLocation = location.reverse('16.0425714,108.219962')
 print("Address : ",getLocation.address)
 print("Latitude : ",getLocation.latitude)
 print("Longitude : ",getLocation.longitude)
