5a98ce79436fa3dff1421eb6810a827e3cce5aea
Thanh Tuyen <dttuyen260701@gmail.com>
2023-04-27 23:48:08 +0700

    add result after processing data

--- CrawlData/TestLocation.py
+++
@@ -1,7 +0,0 @@
-from geopy.geocoders import Nominatim
-location = Nominatim(user_agent="GetLoc", timeout = 10000)
-getLocation = location.reverse('16.0425714,108.219962')
-print("Address : ",getLocation.address)
-print("Latitude : ",getLocation.latitude)
-print("Longitude : ",getLocation.longitude)
-print("Altitude : ",getLocation.altitude)
