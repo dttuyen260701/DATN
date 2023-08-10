872e6a8a4ed52e42ad84b716fab2639151007f03
tuyen.dang <dttuyen260701@gmail.com>
2023-08-10 10:15:11 +0700

    setup Hilt Testing

---
+++ RealEstateApp/app/src/androidTest/java/com/example/realestateapp/AppTest.kt
@@ -0,0 +1,24 @@
+package com.example.realestateapp
+
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import com.example.realestateapp.ui.MainActivity
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Rule
+import org.junit.Test
+
+
+@HiltAndroidTest
+class AppTest {
+
+    @get:Rule(order = 0)
+    var hiltRule = HiltAndroidRule(this)
+
+    @get:Rule(order = 1)
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
+
+    @Test
+    fun test1() {
+        // TODO: Add navigation tests
+    }
+}
